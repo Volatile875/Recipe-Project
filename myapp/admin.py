9c4d0a7d8f973eabaf586b7f38d6695db51bf5bf
Volatile875 <pratik.pt.thakur@gmail.com>
2025-09-04 11:30:04 +0530

    second commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from .models import Recepie
 
-# Register your models here.
-from .models import *
-
-admin.site.register(Recepie)
+@admin.register(Recepie)
+class RecepieAdmin(admin.ModelAdmin):
+    list_display = ("id", "recepie_name", "recepie_description", "recepie_image")
+    search_fields = ("recepie_name", "recepie_description")
