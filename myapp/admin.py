7f56a98db00cdf42423de961668410e40d16ba1a
Volatile875 <pratik.pt.thakur@gmail.com>
2025-09-29 02:27:51 +0530

    WIP: backup before restore (auto snapshot)

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Recepie
 
-@admin.register(Recepie)
-class RecepieAdmin(admin.ModelAdmin):
-    list_display = ("id", "recepie_name", "recepie_description", "recepie_image")
-    search_fields = ("recepie_name", "recepie_description")
+# Register your models here.
+from .models import *
+
+admin.site.register(Recepie)
